bdd491a2e5735adf7c47a685af90d4469e20e7f1
Erik Bagaev <BAGAEFF.ERIK@MAIL.RU>
2022-12-24 15:36:51 +0300

    Задача 3. Добавление слова в словарь и показ его в списке слов

--- lib/newWord_screen/newWordScreenWidget.dart
+++ lib/newWord_screen/newWordScreenWidget.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:iron_dictonary/home_page/home_page.dart';
 
 class NewWordScreenWidget extends StatefulWidget {
-  const NewWordScreenWidget({Key? key}) : super(key: key);
+  NewWordScreenWidget({Key? key}) : super(key: key);
 
   @override
   _NewWordScreenWidgetState createState() => _NewWordScreenWidgetState();
 }
 
 class _NewWordScreenWidgetState extends State<NewWordScreenWidget> {
+  final controller1 = TextEditingController();
+  final controller2 = TextEditingController();
+
+  void buttonTab() {
+    words.add(Word('Слово' + ((words.length) + 1).toString(),
+        [controller1.text, controller2.text], ['Пример 1', 'Пример 2']));
+    controller1.text = "";
+    controller2.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          TextWidget(str: "Осетинский"),
-          TextWidget(str: "Русский"),
-          ElevatedButton(onPressed: () {}, child: Text('Добавить'))
+          TextWidget(
+            str: "Осетинский",
+            controller: controller1,
+          ),
+          TextWidget(
+            str: "Русский",
+            controller: controller2,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                buttonTab();
+              },
+              child: Text('Добавить'))
         ],
       ),
     );
@@ -23,10 +44,12 @@ class _NewWordScreenWidgetState extends State<NewWordScreenWidget> {
 }
 
 class TextWidget extends StatelessWidget {
+  final controller;
   String str = "";
   TextWidget({
     Key? key,
     required this.str,
+    required this.controller,
   }) : super(key: key);
 
   @override
@@ -34,6 +57,7 @@ class TextWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
+        controller: controller,
         clipBehavior: Clip.antiAlias,
         autocorrect: false,
         decoration: InputDecoration(
